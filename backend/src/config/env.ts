b8aa88bea2e8202b65cf7d92cdd8beccca45c192
kuppam kamalesh <kuppamkamalesh2@gmail.com>
2025-09-01 14:58:10 +0530

    Fixed auth.ts and env.ts

--- backend/src/config/env.ts
+++ backend/src/config/env.ts
@@ -1,10 +1,26 @@
+// import dotenv from "dotenv";
+// dotenv.config();
+
+// export const config = {
+//   PORT: process.env.PORT || 4000,
+//   MONGO_URI: process.env.MONGO_URI || "",
+//   JWT_SECRET: process.env.JWT_SECRET || "default_secret",
+//   JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN || "15m",
+//   GMAIL_USER: process.env.GMAIL_USER || "",
+//   GMAIL_PASS: process.env.GMAIL_PASS || "",
+//   GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID || "",
+//   OTP_EXP_MINUTES: Number(process.env.OTP_EXP_MINUTES) || 10,
+//   OTP_RESEND_COOLDOWN_SECONDS:
+//     Number(process.env.OTP_RESEND_COOLDOWN_SECONDS) || 60,
+// };
+
 import dotenv from "dotenv";
 dotenv.config();
 
 export const config = {
   PORT: process.env.PORT || 4000,
   MONGO_URI: process.env.MONGO_URI || "",
-  JWT_SECRET: process.env.JWT_SECRET || "default_secret",
+  JWT_SECRET: process.env.JWT_SECRET || "default_secret", // always string
   JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN || "15m",
   GMAIL_USER: process.env.GMAIL_USER || "",
   GMAIL_PASS: process.env.GMAIL_PASS || "",
